31758fd911df1ad53cb46db812a9f333d275f021
JSU <levi_holt@yahoo.com>
2019-02-14 14:03:50 -0600

    Progress in class

--- app/src/main/java/com/example/jsu/project1/Calculator.java
+++ app/src/main/java/com/example/jsu/project1/Calculator.java
@@ -19,5 +19,19 @@ public class Calculator extends AppCompatActivity {
 
     public void onClick(View v) {
         String id = (v.getResources().getResourceName(v.getId())).split("/")[1];
+        System.out.println(id);
+    }
+
+    public int multiply(int a, int b) {
+        return 0;
+    }
+    public int divide(int a, int b) {
+        return 0;
+    }
+    public int add(int a, int b) {
+        return 0;
+    }
+    public int subtract(int a, int b) {
+        return 0;
     }
 }
